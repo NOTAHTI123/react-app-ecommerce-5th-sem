78424426420a686dd5f8d24e852d1d52068eca00
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2023-12-02 21:25:01 +0500

    main-app

--- ecommerce-app-main/src/components/Home/Home.jsx
+++ ecommerce-app-main/src/components/Home/Home.jsx
@@ -4,8 +4,36 @@ const Home = () => {
     return (
         <div className="main-container-home">
             <div className="banner-div-home">
-                <img src="https://www.elo.shopping/cdn/shop/files/Fashion_Sale_Promotion_Instagram_Post_1800_x_600_px_8afe7888-8ac7-4e51-b8de-562f34ef6216.jpg?v=1701409416&width=2000" alt="#" />
+                <img src="https://www.elo.shopping/cdn/shop/files/Fashion_Sale_Promotion_Instagram_Post_1800_x_600_px_8afe7888-8ac7-4e51-b8de-562f34ef6216.jpg?v=1701409416&width=2000" alt="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/main-woman-banner.jpg" />
             </div>
+            <section className="men-women-kid-fashion-home">
+                <div className="heading-home">
+                    <h2>Latest in Men&apos;s Fashion</h2>
+                </div>
+                <div className="product-container">
+                    <div className="product-default-home">
+                        <img src="https://www.elo.shopping/cdn/shop/files/3265d.jpg?v=1701495218&width=713" alt="" />
+                        <a href="#"><h3>Salesianer Men&apos;s Short Sleeves Minor Fault Polo Shirt</h3></a>
+                        <p>Rs 599.00</p>
+                    </div>
+
+                    <div className="product-default-home">
+                        <img src="https://www.elo.shopping/cdn/shop/files/3265d.jpg?v=1701495218&width=713" alt="" />
+                        <a href="#"><h3>Salesianer Men&apos;s Short Sleeves Minor Fault Polo Shirt</h3></a>
+                        <p>Rs 599.00</p>
+                    </div>
+                    <div className="product-default-home">
+                        <img src="https://www.elo.shopping/cdn/shop/files/3265d.jpg?v=1701495218&width=713" alt="" />
+                        <a href="#"><h3>Salesianer Men&apos;s Short Sleeves Minor Fault Polo Shirt</h3></a>
+                        <p>Rs 599.00</p>
+                    </div>
+                    <div className="product-default-home">
+                        <img src="https://www.elo.shopping/cdn/shop/files/3265d.jpg?v=1701495218&width=713" alt="" />
+                        <a href="#"><h3>Salesianer Men&apos;s Short Sleeves Minor Fault Polo Shirt</h3></a>
+                        <p>Rs 599.00</p>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
