b20480ca7eea98efb9bdddffeacde8a0717bcf82
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2023-12-03 18:13:53 +0500

    Update

--- ecommerce-app-main/src/components/Home/Home.jsx
+++ ecommerce-app-main/src/components/Home/Home.jsx
@@ -7,31 +7,70 @@ const Home = () => {
                 <img src="https://www.elo.shopping/cdn/shop/files/Fashion_Sale_Promotion_Instagram_Post_1800_x_600_px_8afe7888-8ac7-4e51-b8de-562f34ef6216.jpg?v=1701409416&width=2000" alt="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/main-woman-banner.jpg" />
             </div>
             <section className="men-women-kid-fashion-home">
-                <div className="heading-home">
-                    <h2>Latest in Men&apos;s Fashion</h2>
-                </div>
-                <div className="product-container">
-                    <div className="product-default-home">
-                        <img src="https://www.elo.shopping/cdn/shop/files/3265d.jpg?v=1701495218&width=713" alt="" />
-                        <a href="#"><h3>Salesianer Men&apos;s Short Sleeves Minor Fault Polo Shirt</h3></a>
-                        <p>Rs 599.00</p>
+                <div className="product-container-men">
+                    <div className="heading-home">
+                        <h2>Latest in Men&apos;s Fashion</h2>
+                    </div>
+                    <div className="product-container">
+                        <div className="product-default-home">
+                            <img src="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/male/male-1.jpg" alt="" />
+                            <a href="#"><h3>Salesianer Men&apos;s Short Sleeves Minor Fault Polo Shirt</h3></a>
+                            <p>Rs 599.00</p>
+                        </div>
+    
+                        <div className="product-default-home">
+                            <img src="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/male/male-2.jpg" alt="" />
+                            <a href="#"><h3>Aqua Men&apos;s Minor Fault Thermal Tee Shirt</h3></a>
+                            <p>Rs 599.00</p>
+                        </div>
+                        <div className="product-default-home">
+                            <img src="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/male/male-3.jpg" alt="" />
+                            <a href="#"><h3>Eternity Men&apos;s Formal Button Down Dress Shirt</h3></a>
+                            <p>Rs 599.00</p>
+                        </div>
+                        <div className="product-default-home">
+                            <img src="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/male/male-4.jpg" alt="" />
+                            <a href="#"><h3>Polo Republica Men&apos;s Contrast Raglan Shoulder Minor Fault Terry Zipper Hoodie</h3></a>
+                            <p>Rs 599.00</p>
+                        </div>
+
                     </div>
 
-                    <div className="product-default-home">
-                        <img src="https://www.elo.shopping/cdn/shop/files/3265d.jpg?v=1701495218&width=713" alt="" />
-                        <a href="#"><h3>Salesianer Men&apos;s Short Sleeves Minor Fault Polo Shirt</h3></a>
-                        <p>Rs 599.00</p>
+                    <button className='view-all-button-home' type="button">View All</button>
+
+                </div>
+
+                <div className="product-container-women">
+                    <div className="heading-home">
+                        <h2>Latest in Women&apos;s Fashion</h2>
                     </div>
-                    <div className="product-default-home">
-                        <img src="https://www.elo.shopping/cdn/shop/files/3265d.jpg?v=1701495218&width=713" alt="" />
-                        <a href="#"><h3>Salesianer Men&apos;s Short Sleeves Minor Fault Polo Shirt</h3></a>
-                        <p>Rs 599.00</p>
+                    <div className="product-container">
+                        <div className="product-default-home">
+                            <img src="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/male/male-1.jpg" alt="" />
+                            <a href="#"><h3>High Spirits Women&apos;s Long Sleeve Denim Jumpsuit</h3></a>
+                            <p>Rs 1500</p>
+                        </div>
+    
+                        <div className="product-default-home">
+                            <img src="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/male/male-2.jpg" alt="" />
+                            <a href="#"><h3>East West Women&apos;s Model Girls Printed Short Sleeve Tee Shirt</h3></a>
+                            <p>Rs 769.00</p>
+                        </div>
+                        <div className="product-default-home">
+                            <img src="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/male/male-3.jpg" alt="" />
+                            <a href="#"><h3>East West Women&apos;s Stitched Suit</h3></a>
+                            <p>Rs 2100.00</p>
+                        </div>
+                        <div className="product-default-home">
+                            <img src="https://raw.githubusercontent.com/NOTAHTI123/react-app-ecommerce-5th-sem/main/ecommerce-app-main/src/assets/images/male/male-4.jpg" alt="" />
+                            <a href="#"><h3>East West Women&apos;s 2 Pc Stitched</h3></a>
+                            <p>Rs 2100.00</p>
+                        </div>
+
                     </div>
-                    <div className="product-default-home">
-                        <img src="https://www.elo.shopping/cdn/shop/files/3265d.jpg?v=1701495218&width=713" alt="" />
-                        <a href="#"><h3>Salesianer Men&apos;s Short Sleeves Minor Fault Polo Shirt</h3></a>
-                        <p>Rs 599.00</p>
-                    </div>
+
+                    <button className='view-all-button-home' type="button">View All</button>
+
                 </div>
             </section>
         </div>
